e4cf999f00dfa1146c0112bb92a7279eb8da03cd
Chris Bowdon <chris.bowdon@polecat.com>
2019-06-04 11:30:09 +0100

    Attempt to fix setup.py to correctly state requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
 	name='corextopic',
-	version='1.0.2',
+	version='1.0.3',
 	author='Greg Ver Steeg/Ryan J. Gallagher',
 	author_email='gregv@isi.edu',
 	keywords=['topic model', 'corex', 'anchored corex', 'LDA', 'semi-supervised', 'hierarchical topic model', 'information theory'],
@@ -14,6 +14,13 @@ setuptools.setup(
 	long_description_content_type='text/markdown',
 	url='https://github.com/gregversteeg/corex_topic',
 	packages=setuptools.find_packages(),
+        install_requires=[
+                'joblib >= 0.13.2',
+                'numpy >= 1.16.4',
+                'scikit-learn >= 0.21.2',
+                'scipy >= 1.3.0',
+                'six >= 1.12.0',
+        ],
 	classifiers=(
 		'Programming Language :: Python',
 		'Programming Language :: Python :: 3',
